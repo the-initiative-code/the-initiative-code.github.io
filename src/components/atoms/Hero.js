ebefc6d83e6f5e6cb3166c5cc0c9c5fd22933738
panzerstadt <tliqun@gmail.com>
2018-12-31 21:40:49 +0900

    more styling#

--- src/components/atoms/Hero.js
+++ src/components/atoms/Hero.js
@@ -10,7 +10,11 @@ const HeroText = () => (
   <div className={styles.default}>
     <div className={styles.text}>
       <h1>let us build your digital brand with you.</h1>
-      <h2>we work with you to bring your digital presence to life.</h2>
+      <h4>we work with you to bring your digital presence to life.</h4>
+      <p>
+        brand authenticity is the key factor to driving engagement with your
+        brand. We can help you take the first step.{' '}
+      </p>
       {/* <code>hero text here, use absolute positioning</code>
       <code>
         in the future, I plan to animate the text based on the content behind
